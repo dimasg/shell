e30b7bd1bd8a964d21a9b848203e6cce99e3f2ed
Dmitry Grachev <dimasg@gmail.com>
2025-02-04 17:43:37 +0100

    fish: fish_title bugfix

--- fish/functions/fish_title.fish
+++ fish/functions/fish_title.fish
@@ -11,7 +11,7 @@ function fish_title
     if test $command = "fish"
         # we are sitting at the fish prompt
 
-        if git rev-parse --git-dir > /dev/null ^ /dev/null
+        if git rev-parse --git-dir &> /dev/null
             # we are inside a git directory, so use the name of the repo as the terminal title
 
             set -l git_dir (git rev-parse --git-dir)
